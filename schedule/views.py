d80f1c8ee86e1ce4354e62baa75fe0d31d11a595
andrtemp <andrew.yaroshevskyy@gmail.com>
2019-11-14 11:27:05 +0200

    FIRST styles applied

--- schedule/views.py
+++ schedule/views.py
@@ -5,7 +5,7 @@ from .models import Class
 def index(request):
     classes = Class.objects.all()[:5]
     context = { 'latest_question_list': classes }
-    return render(request, 'layout.html', context)
+    return render(request, 'pages/main.html', context)
 
 def lesson(request, lesson_id):
     return HttpResponse("You're looking at question %s." % lesson_id)
